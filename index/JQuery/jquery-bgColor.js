26441f04a23c5639c08f64fb72cda9e9989820ad
2499538506 <2499538506@qq.com>
2020-06-04 22:57:47 +0800

    enter

--- index/JQuery/jquery-bgColor.js
+++ index/JQuery/jquery-bgColor.js
@@ -1,11 +1,11 @@
-(function ($)
+(function($)
 {
     $.fn.bgColor = function(yanse)
     {
         this.css('backgroundColor',yanse);
 
-        //返回调用这个方法的jQuery本身
-        return this;
+        return false;
     }
 
-}(jQuery));
+}(jQuery))
+
