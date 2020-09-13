b131458d02744b097f6a6fd2d852395b73e316ff
hollowly <2499538506@qq.com>
2020-09-13 18:23:27 +0800

    enter

--- index/node.js/04.使用服务器输出hello world.js
+++ index/node.js/04.使用服务器输出hello world.js
@@ -15,6 +15,6 @@ server.on('request',function(request, response) {
     response.end();
 });
 
-server.listen(3000,function() {
+server.listen(3002,function() {
     console.log('服务器启动成功了，可以通过http://127.0.0.1:3000/ 来进行访问');
 });
