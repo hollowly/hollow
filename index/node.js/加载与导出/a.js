0faa95f8fc92e3f4819382ca4da12c9ff48f64cf
hollowly <2499538506@qq.com>
2020-09-13 20:38:43 +0800

    nter

---
+++ index/node.js/加载与导出/a.js
@@ -0,0 +1,16 @@
+// require 方法有两种作用：
+    // 1.加载文件模块并执行里面的代码
+    // 2.拿到被加载文件模块导出接口对象
+
+    // 在美国文件模块中都提供了一个对象。exports
+    // exports 默认是一个空对象
+    // 你要做的就是把所有需要被外部访问的成员。。。
+
+
+var bExports = require('./b.js');
+
+console.log(bExports.foo);
+
+console.log(bExports.add(10,3000));
+
+console.log(bExports.ages);
---
+++ index/node.js/加载与导出/b.js
@@ -0,0 +1,17 @@
+var foo = '123';
+
+// console.log(foo);
+
+exports.foo = 'hello';
+
+exports.add = function(x, y) {
+    return x + y;
+}
+
+function add(x, y) {
+    x - y;
+}
+
+age = '10';
+
+exports.ages = age;
