1bffbabf8b24233cc3f5129015a0d2edbc7aa73a
hollowly <2499538506@qq.com>
2020-10-30 21:15:20 +0800

    enter

--- index/Vue/learn-router/src/App.vue
+++ index/Vue/learn-router/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
@@ -11,7 +12,12 @@ export default {
 </script>
 
 <style>
-  body {
-    background: url('img');
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
