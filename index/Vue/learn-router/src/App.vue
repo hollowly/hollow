3f2c2360333bed835a6057355052e043de9ce0d8
hollowly <2499538506@qq.com>
2020-10-31 22:25:16 +0800

    enter

--- index/Vue/learn-router/src/App.vue
+++ index/Vue/learn-router/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
 		<h1>我是App组件</h1>
-		<h2><router-link to='cpn1'>cpn1</router-link></h2>
-		<h2><router-link to='cpn2'>cpn2</router-link></h2>
+		<!-- <h2><router-link to='cpn1' tag="button" replace>cpn1</router-link></h2>
+		<h2><router-link to='cpn2' tag='button' replace>cpn2</router-link></h2> -->
+		<button @click='cpn1Click'>cpn1</button>
+		<button @click='cpn2Click'>cpn2</button>
 		<router-view></router-view>
 		<h1>hello world</h1>
   </div>
@@ -11,6 +13,14 @@
 <script>
 export default {
 	name: 'App',
+	methods: {
+		cpn1Click() {
+			this.$router.push('/cpn1');
+		},
+		cpn2Click() {
+			this.$router.push('/cpn2');
+		}
+	},
 }
 </script>
 
@@ -18,5 +28,17 @@ export default {
 body {
 	background: url('./img/10.jpg');
 	background-position: center 0%;
+}
+button {
+	background: pink;
+	border: none;
+	outline: none;
+	cursor:pointer;
+	width: 100px;
+	height: 30px;
+}
+ .active {
+	 color: #FF0000;
+	 border: 1px solid red;
 }
 </style>
