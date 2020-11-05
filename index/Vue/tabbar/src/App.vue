e4ed1cf090596c78bff877199b73b1ae26523977
hollowly <2499538506@qq.com>
2020-11-05 21:37:19 +0800

    enter

--- index/Vue/tabbar/src/App.vue
+++ index/Vue/tabbar/src/App.vue
@@ -7,22 +7,24 @@
 				<img src="./assets/img/tabbar/home_active.png" slot='item-icon-active'>
 				<div slot='item-text'>首页</div>
 			</tabbaritem>
+
 			<tabbaritem>
 				<img src="./assets/img/tabbar/categories.png" alt="" slot='item-icon'>
 				<img src="./assets/img/tabbar/categories_active.png" slot='item-icon-active'>
 				<div slot='item-text'>更多</div>
 			</tabbaritem>
+
 			<tabbaritem>
 				<img src="./assets/img/tabbar/shopcart.png" alt="" slot='item-icon'>
 				<img src="./assets/img/tabbar/shopcart_active.png" slot='item-icon-active'>
 				<div slot='item-text'>购物车</div>
-				</tabbaritem>
+			</tabbaritem>
+
 			<tabbaritem>
 				<img src="./assets/img/tabbar/profile.png" alt="" slot='item-icon'>
 				<img src="./assets/img/tabbar/profile_active.png" slot='item-icon-active'>
 				<div slot='item-text'>我的</div>
-				</tabbaritem>
-
+			</tabbaritem>
 		</tabbar>
 	</div>
 </template>
