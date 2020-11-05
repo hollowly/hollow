6a131fdd00b8ca8e3653db1e8212147712ad7a5e
hollowly <2499538506@qq.com>
2020-11-05 21:36:20 +0800

    enter

---
+++ index/Vue/tabbar/src/views/categories/categories.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+	<div>
+		catgories
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	},
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ index/Vue/tabbar/src/views/home/home.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+	<div>
+		home
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	},
+
+}
+
+</script>
+
+<style>
+
+</style>
