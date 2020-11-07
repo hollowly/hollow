50cf0622323fa65d3486af1745769bcd32cfa99a
hollowly <2499538506@qq.com>
2020-11-07 19:57:51 +0800

    enter

--- index/Vue/vue-cli3/src/App.vue
+++ index/Vue/vue-cli3/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
-
+		<cpn path='/home'></cpn>
   </div>
 </template>
 
 <script>
-
+import cpn from './components/cpn'
 
 export default {
 	name: 'App',
+
+	
+	components: {
+		cpn,
+	}
 	
 }
 </script>
