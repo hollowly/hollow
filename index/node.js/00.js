224a657472ef658272ab2e50531a58453e362234
hollowly <2499538506@qq.com>
2020-11-12 22:07:44 +0800

    enter

--- index/node.js/00.js
+++ index/node.js/00.js
@@ -1,15 +1,30 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer();
 
 server.on('request',(req, res) => {
+	const url = req.url;
 
-	if(req.url === '/') {
-		res.setHeader('Content-type','text/html; charset=utf-8')
-		res.end('你好，世界')
-	} else if(req.url === '/login') {
-		res.setHeader('Content-type','text/html; charset=utf-8')
-		res.end('<h1>欢迎登录</h1>')
+	if(url === '/') {
+		fs.readFile('./index/index.html',(err, data) => {
+			if(err) {
+				res.setHeader('Content-type','text/plain; charset=utf-8')
+				res.end('读取失败');
+			} else {
+				res.end(data)
+			}
+		})
+	} else if(url === '/img') {
+		fs.readFile('./img/10.jpg',(err, data) => {
+			if(err) {
+				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				res.end('读取失败')
+			} else {
+				res.setHeader('Content-type','image/jpeg')
+				res.end(data)
+			}
+		}) 
 	} else {
 		res.end('not font 404')
 	}
