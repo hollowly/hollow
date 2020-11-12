b4e3cb688eae0fabce281089f125a06d1265bcbf
hollowly <2499538506@qq.com>
2020-11-12 17:45:37 +0800

    enter

--- index/Vue/learn-axios/src/network/request.js
+++ index/Vue/learn-axios/src/network/request.js
@@ -68,9 +68,26 @@ export function request(config) {
 		baseURL: 'http://123.207.32.32:8000',
 		timeout:5000
 	})
-	
-	// 2.axios的拦截器
 
+	// 2.axios的拦截器
+		// 2.1请求拦截的作用
+		// 2.1.1.config中的一些信息不符合服务器的要去
+		// 2.1.2.每次发送网络请求时,都希望在界面中显示一个请求的图标
+		// 2.1.3.某些网络请求(登录(token))，会携带一些特殊信息
+	instance.interceptors.request.use(config => {
+		console.log(config);
+		return config
+	}, err => {
+		console.log(err);
+	})
+
+	// 2.2响应拦截
+	instance.interceptors.request.use(res => {
+		// console.log(res);
+		return res.data
+	}, err => {
+		console.log(err);
+	})
 
 
 	// 3.发送真正的网络请求
