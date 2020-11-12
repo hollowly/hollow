99cdb3dc5d46fd6361cacb3cc7b980e4bcf17440
hollowly <2499538506@qq.com>
2020-11-12 15:37:17 +0800

    enter

--- index/Vue/learn-axios/src/main.js
+++ index/Vue/learn-axios/src/main.js
@@ -12,7 +12,8 @@ new Vue({
   render: h => h(App)
 })
 
-
+//---------------------------------------------------------------------------
+// 1.axios的基本使用
 // axios({
 // 	url:'http://123.207.32.32:8000/home/multidata',
 // 	method:'get'
@@ -31,17 +32,39 @@ new Vue({
 // axios({
 // 	url:'http://123.207.32.32:8000/home/',
 // 	// 专门针对get请求参数拼接
-// 	type:'sell',
-// 	page:'1'
+// 	params: {
+// 		type:'pop',
+// 		page:'1'
+// 	}
 // }).then(res => {
 // 	console.log(res);
 // })
 
-axios({
-	url:'http://123.207.32.32:8000/home',
-	type:'pop',
-	page:'2',
-	method:'pop',
-}).then(res => {
-	console.log(res);
+//---------------------------------------------------------------------------
+// 2.axios发送并发请求
+// axios.all([axios({
+// 	url:"http://aaaaa/home"
+// }),axios({
+// 	url:'http://bbbb/home',
+// 	params: {
+// 		type:'data',
+// 		page:4
+// 	}
+// })]).then(res => {
+// 	console.log(res);
+// })
+
+
+//---------------------------------------------------------------------------
+// 3.axios全局配置
+axios.defaults.baseURL = 'http://123.207.32.32:8000'
+axios.defaults.timeout = 5000
+
+
+axios.all([axios({
+	url:'/home/multidata'
+}),axios({
+	url:'/home/data'
+})]).then(results => {
+	console.log(results);
 })
