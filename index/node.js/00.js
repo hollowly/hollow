b5bcfa9cfb78877b5ab2232ebea8f4c67a59773d
hollowly <2499538506@qq.com>
2020-11-13 22:26:42 +0800

    enter

--- index/node.js/00.js
+++ index/node.js/00.js
@@ -1,20 +1,32 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
+
+
 
 http.createServer((req, res) => {
-	const url = req.url;
-	if(url === '/') {
-		fs.readFile('./index/index.html',(err, data) => {
+	const urlPath = url.parse(req.url,true).pathname
+
+	if(urlPath === '/') {
+		fs.readFile('./index2/index.html',(err, date) => {
 			if(err) {
 				res.setHeader('Content-Type','text/plain;charset=utf-8')
 				return res.end('读取失败')
-			} 
-			res.end(data)
+			}
+			res.end(date)
 		})
-	} else if(url.indexOf('/index/static/') === 0) {
-		fs.readFile('.' + url,(err, data) => {
+	} else if(urlPath === '/static/login') {
+		fs.readFile('./index2/static/login.html',(err, data) => {
 			if(err) {
 				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				return res.end('读取失败')
+			}
+			res.end(data)
+		})
+	}else if(urlPath === '/static/img') {
+		fs.readFile('./index2/static/girl.jpg',(err, data) => {
+			if(err) {
+				res.setHeader('Content-Type','image/jpeg')
 				return res.end('读取失败')
 			}
 			res.end(data)
