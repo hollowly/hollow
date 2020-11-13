92af64279b27829c00288d7438387947968619bc
hollowly <2499538506@qq.com>
2020-11-13 14:52:28 +0800

    enter

--- index/node.js/15..js
+++ index/node.js/15..js
@@ -1,7 +1,18 @@
+const { fstat } = require('fs');
 const http = require('http')
+const fs = require('fs')
 
 http.createServer((req, res) => {
-	res.end('hello nodejs')
+	const url = req.url;
+
+	if(url === '/') {
+		fs.readFile('./index/index.html',(err, data) => {
+			if(err) {
+				return res.end('读取失败')
+			}
+			res.end(data)
+		}) 
+	}
 }).listen(3000,() => {
 	console.log('server is running http:127.0.0.1:3000');
 })
