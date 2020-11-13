17e914a08b7a7613845980d865af3750c68f6e04
hollowly <2499538506@qq.com>
2020-11-13 11:13:21 +0800

    enter

--- index/node.js/00.js
+++ index/node.js/00.js
@@ -3,31 +3,36 @@ const fs = require('fs')
 
 const server = http.createServer();
 
-server.on('request',(req, res) => {
+server.on('request',(req,res) => {
 	const url = req.url;
 
-	if(url === '/') {
+	if(url ==='/') {
 		fs.readFile('./index/index.html',(err, data) => {
 			if(err) {
-				res.setHeader('Content-type','text/plain; charset=utf-8')
-				res.end('读取失败');
+				res.setHeader('Content-Type','text/html;charset=utf-8')
+				res.end('读取失败')
 			} else {
+				res.setHeader('Content-Type','text/html;charset=utf-8')
 				res.end(data)
 			}
 		})
 	} else if(url === '/img') {
 		fs.readFile('./img/10.jpg',(err, data) => {
 			if(err) {
-				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				res.setHeader('Content-Type','text/html;charset=utf-8')
 				res.end('读取失败')
 			} else {
-				res.setHeader('Content-type','image/jpeg')
+				res.setHeader('Content-Type','image/jpeg')
 				res.end(data)
 			}
-		}) 
+		})
 	} else {
 		res.end('not font 404')
 	}
-}).listen(3000,() => {
-	console.log('server is runing');
+
+
+
+
+}).listen(3000,function() {
+	console.log('server is runing http://127.0.0.1:3000');
 })
