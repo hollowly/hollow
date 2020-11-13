6ba34bb39c4f44f1bd0e52f8ae1edfd49791a8b0
hollowly <2499538506@qq.com>
2020-11-13 22:03:58 +0800

    enter

--- index/node.js/15.处理网站中的静态资源.js
+++ index/node.js/15.处理网站中的静态资源.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const fs = require('fs')
+const template = require('art-template')
 const url = require('url')
-var arr =  [
+var comments =  [
 	{name:'张三',message:'今天天气真不错',date:'2020-11-5'},
 	{name:'李四',message:'今天天气真不错+1',date:'2020-11-7'},
 	{name:'王五',message:'今天天气真不错+2',date:'2020-11-8'},
@@ -16,7 +17,12 @@ http.createServer((req, res) => {
 			if(err) {
 				return res.end('读取失败')
 			}
-			res.end(data)
+			// 使用 art-template来对数据进行render(渲染)
+			var htmlStr = template.render(data.toString(), {
+				comments:comments
+			})
+
+			res.end(htmlStr)
 		}) 
 	} else if(pathname === '/post') {
 		fs.readFile('./index/post.html',(err, data) => {
@@ -33,14 +39,13 @@ http.createServer((req, res) => {
 			res.end(data)
 		})
 	} else if(pathname === '/pinglun') {
-		res.statusCode = 302
-		res.setHeader('location','/index/static/vue/index.js')
-		var obj2 = {};
-		obj2['name'] = urlObj.query.name
-		obj2['message'] = urlObj.query.message
-		obj2['data'] = '2020-11-13'
-		arr.unshift(obj2)
+		var arr = urlObj.query
+		arr.date = '2020-11-13'
+		comments.unshift(arr)
 		console.log(arr);
+		// 重定向到 / 路径中，然后客户端收到服务器的响应状态码是302，就会自动去响应头去找 location ，然后对该地址发起新的请求
+		res.statusCode = 302
+		res.setHeader('location','/')
 		res.end()
 	} else {
 		fs.readFile('./index/404.html',(err, data) => {
