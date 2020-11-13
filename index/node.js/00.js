a895b2e64683fe997cc4633979c0f74c3d3a6c1e
hollowly <2499538506@qq.com>
2020-11-13 15:22:55 +0800

    enter

--- index/node.js/00.js
+++ index/node.js/00.js
@@ -1,38 +1,23 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer();
-
-server.on('request',(req,res) => {
+http.createServer((req, res) => {
 	const url = req.url;
-
-	if(url ==='/') {
+	if(url === '/') {
 		fs.readFile('./index/index.html',(err, data) => {
 			if(err) {
-				res.setHeader('Content-Type','text/html;charset=utf-8')
-				res.end('读取失败')
-			} else {
-				res.setHeader('Content-Type','text/html;charset=utf-8')
-				res.end(data)
-			}
+				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				return res.end('读取失败')
+			} 
+			res.end(data)
 		})
-	} else if(url === '/img') {
-		fs.readFile('./img/10.jpg',(err, data) => {
+	} else if(url.indexOf('/index/static/') === 0) {
+		fs.readFile('.' + url,(err, data) => {
 			if(err) {
-				res.setHeader('Content-Type','text/html;charset=utf-8')
-				res.end('读取失败')
-			} else {
-				res.setHeader('Content-Type','image/jpeg')
-				res.end(data)
+				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				return res.end('读取失败')
 			}
+			res.end(data)
 		})
-	} else {
-		res.end('not font 404')
 	}
-
-
-
-
-}).listen(3000,function() {
-	console.log('server is runing http://127.0.0.1:3000');
-})
+}).listen(3000)
--- index/node.js/15.处理网站中的静态资源.js
+++ index/node.js/15.处理网站中的静态资源.js
@@ -1,10 +1,8 @@
-const { fstat } = require('fs');
 const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, res) => {
 	const url = req.url;
-
 	if(url === '/') {
 		fs.readFile('./index/index.html',(err, data) => {
 			if(err) {
@@ -12,6 +10,13 @@ http.createServer((req, res) => {
 			}
 			res.end(data)
 		}) 
+	} else if(url.indexOf('/index/static/') === 0) {
+		fs.readFile('.' + url,(err,data) => {
+			if(err) {
+				return res.end('读取失败')
+			} 
+			res.end(data)
+		})
 	}
 }).listen(3000,() => {
 	console.log('server is running http:127.0.0.1:3000');
