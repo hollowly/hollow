59172d0596c5d2f89ea02a75164714b29ea0f9c0
hollowly <2499538506@qq.com>
2020-11-13 16:53:58 +0800

    enter

--- index/node.js/15.处理网站中的静态资源.js
+++ index/node.js/15.处理网站中的静态资源.js
@@ -3,13 +3,20 @@ const fs = require('fs')
 
 http.createServer((req, res) => {
 	const url = req.url;
-	if(url === '/') {
+	if(url === '/?' || url === '/') {
 		fs.readFile('./index/index.html',(err, data) => {
 			if(err) {
 				return res.end('读取失败')
 			}
 			res.end(data)
 		}) 
+	} else if(url === '/post') {
+		fs.readFile('./index/post.html',(err, data) => {
+			if(err) {
+				return	res.end('404 Not Font.')
+			}
+			res.end(data)
+		})
 	} else if(url.indexOf('/index/static/') === 0) {
 		fs.readFile('.' + url,(err,data) => {
 			if(err) {
@@ -18,7 +25,12 @@ http.createServer((req, res) => {
 			res.end(data)
 		})
 	} else {
-		res.end('404 Not Font.')
+		fs.readFile('./index/404.html',(err, data) => {
+			if(err) {
+				return	res.end('404 Not Font.')
+			}
+			res.end(data)
+		})
 	}
 }).listen(3000,() => {
 	console.log('server is running http:127.0.0.1:3000');
