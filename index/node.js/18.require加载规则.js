47d2ce9e42867f515bb72e9cf5f96f4fb2e71bab
hollowly <2499538506@qq.com>
2020-11-15 15:57:53 +0800

    enter

--- index/node.js/18.require加载规则.js
+++ index/node.js/18.require加载规则.js
@@ -1,4 +1,7 @@
-
+// 核心模块的本质是文件
+// 核心模块文件已经被编译到了二进制文件中，我们只需要按照名字来加载就可以了
+require('fs')
+require('http')
 
 
 //第三方模块
@@ -11,6 +14,16 @@
 // node_modules/art-template/package.json文件
 // nodt_modules/art-template/package.json文件中的main属性
 // main属性中就记录了art-template的入口模块
+// 然后加载使用这个第三方包
+// 实际上最终加载的还是文件
+
+// 如果package.json文件不存在或者main指定的入口是错的
+// 则node会自动找该目录下的index.js
+// 也就是说index.js会作为一个默认备选项
+
+// 如果以上所有任何一个条件都不成立，则会进入上一级目录中的 node_modules目录查找
+// 如果上一级还没有，则继续玩上上一级查找
+// 直到当前磁盘根目录还没找到，最后报错：	can not find module xxx
 
 const template = require('art-template')
 
