6d4cd095a7086c8730c1e6b57e2f4b077c855d1d
hollowly <2499538506@qq.com>
2020-11-15 16:36:24 +0800

    enter

---
+++ index/node.js/19.npm包说明.js
@@ -0,0 +1,11 @@
+// 建议每个项目都有一个 package.json 文件，
+// 这个文件可以通过npm init 的方式来初始化
+
+
+// 在你npm install xxx 的时候，
+// package.json会保存安装的依赖信息，
+// 同时生成node_modules文件夹来保存安装的依赖
+
+
+// 如果你的 node_modules删除了也不用担心，
+// 只需要npm install就会把package.json中的所有依赖项都下载回来
