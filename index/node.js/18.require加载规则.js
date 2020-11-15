f38c9918779deab40cfa418149b20e23c538fd41
hollowly <2499538506@qq.com>
2020-11-15 15:40:58 +0800

    enter

---
+++ index/node.js/18.require加载规则.js
@@ -0,0 +1,14 @@
+
+
+
+//第三方模块
+//l凡是第三方模块都必须通过npm 来下载
+//使用的时候就可以通过require('包名'）的方式来进行加载才可以使用
+//不可能有任何一个第三方包和核心模块的名字是—样的
+//既不是核心模块、也不是路径形式的模块
+// 先找到当前文件所处目录中的node_modules目录
+// node_modules/art-template
+// node_modules/art-template/package.json文件
+// nodt_modules/art-template/package.json文件中的main属性
+// main属性中就记录了art-template的入口模块
+
