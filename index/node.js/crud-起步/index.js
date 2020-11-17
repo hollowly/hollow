52f5eacfcabe183fbac2f246bd02339575de1dee
hollowly <2499538506@qq.com>
2020-11-17 21:13:06 +0800

    enter

--- index/node.js/crud-起步/index.js
+++ index/node.js/crud-起步/index.js
@@ -7,8 +7,8 @@ app.engine('html',require('express-art-template'))
 app.use('/public/',express.static('./public/'))
 app.use('/node_modules/',express.static('../../../node_modules/'))
 
-router(app)
-
+// 把路由容器挂载到 app 服务中
+app.use(router)
 
 app.listen(3000,() => {
 	console.log('server is runnig....');
--- index/node.js/crud-起步/router.js
+++ index/node.js/crud-起步/router.js
@@ -1,10 +1,18 @@
 
 	const fs = require('fs')
 
-module.exports = function(app) {
-	app.get('/', (req, res) => {
-		// readFile 的第二个参数是可选的，传入utf8就是告诉它把读取到的文件直接按照 utf8 编码
-		// 除了这样来转换之外，也可以通过data.tostring()的方式
+	// express提供了一种封装专门用来包装路由的
+	const express = require("express")
+
+	// 1.创建路由容器
+	const router = express.Router()
+	
+	router.get('/',(req, res) => {
+		res.send('index page')
+	})
+
+	// 2.把路由都挂载到 router 容器中
+	router.get('/students',(req, res) => {
 		fs.readFile('./db.json','utf8',(err, data) => {
 			if(err) {
 				return res.status(500).send('server error.')
@@ -14,30 +22,72 @@ module.exports = function(app) {
 					'苹果',
 					'香蕉',
 					'橘子',
-					'西瓜'
+					'西瓜',
 				],
-				// 从文件中读取到的数据一定是字符串，所以一定要手动转成对象
 				info: JSON.parse(data).student
 			})
 		})
 	})
 
-	app.get('/students', (err, data) => {
+	router.get('/students/new',(req, res) => {
+		res.render('new.html')
+	})
+
+	router.post('/students/new',(req, res) => {
 		
 	})
-	app.get('/students', (err, data) => {
+
+	router.get('/students/edit',(req, res) => {
 		
 	})
-	app.get('/students', (err, data) => {
+	router.post('/students/edit',(req, res) => {
 		
 	})
-	app.get('/students', (err, data) => {
+	router.get('/students/delete',(req, res) => {
 		
 	})
-	app.get('/students', (err, data) => {
+
+
+	module.exports = router
+
+	// 这样不方便
+// module.exports = function(app) {
+// 	app.get('/', (req, res) => {
+// 		// readFile 的第二个参数是可选的，传入utf8就是告诉它把读取到的文件直接按照 utf8 编码
+// 		// 除了这样来转换之外，也可以通过data.tostring()的方式
+// 		fs.readFile('./db.json','utf8',(err, data) => {
+// 			if(err) {
+// 				return res.status(500).send('server error.')
+// 			}
+// 			res.render('index.html', {
+// 				fruits: [
+// 					'苹果',
+// 					'香蕉',
+// 					'橘子',
+// 					'西瓜'
+// 				],
+// 				// 从文件中读取到的数据一定是字符串，所以一定要手动转成对象
+// 				info: JSON.parse(data).student
+// 			})
+// 		})
+// 	})
+
+// 	app.get('/students', (err, data) => {
 		
-	})
-	app.get('/students', (err, data) => {
+// 	})
+// 	app.get('/students', (err, data) => {
 		
-	})
-}
+// 	})
+// 	app.get('/students', (err, data) => {
+		
+// 	})
+// 	app.get('/students', (err, data) => {
+		
+// 	})
+// 	app.get('/students', (err, data) => {
+		
+// 	})
+// 	app.get('/students', (err, data) => {
+		
+// 	})
+// }
