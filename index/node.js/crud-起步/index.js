3df116adce9012a2de00431e2443f0696f9431ed
hollowly <2499538506@qq.com>
2020-11-17 12:44:08 +0800

    enter

--- index/node.js/crud-起步/index.js
+++ index/node.js/crud-起步/index.js
@@ -15,15 +15,15 @@ app.get('/', (req, res) => {
 			'西瓜'
 		],
 		info: [
-			{'id':'1001','name':'hollow','class':'软件19-4','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1002','name':'aaaa','class':'软件19-5','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1003','name':'bbbb','class':'软件19-2','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1004','name':'cccc','class':'软件19-4','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1005','name':'dddd','class':'软件19-3','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1006','name':'eeee','class':'软件19-2','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1007','name':'ffff','class':'软件19-4','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1008','name':'gggg','class':'软件19-6','hobbys':'吃饭、睡觉、敲代码'},
-			{'id':'1009','name':'hhhh','class':'软件19-1','hobbys':'吃饭、睡觉、敲代码'}
+			{id:'1001',name:'hollow',class:'软件19-4',age:18,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1002',name:'aaaa',class:'软件19-5',age:19,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1003',name:'bbbb',class:'软件19-2',age:20,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1004',name:'cccc',class:'软件19-4',age:18,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1005',name:'dddd',class:'软件19-3',age:18,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1006',name:'eeee',class:'软件19-2',age:19,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1007',name:'ffff',class:'软件19-4',age:19,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1008',name:'gggg',class:'软件19-6',age:19,hobbys:'吃饭、睡觉、敲代码'},
+			{id:'1009',name:'hhhh',class:'软件19-1',age:20,hobbys:'吃饭、睡觉、敲代码'}
 		]
 	})
 })
