cfb5eb2e6e78355cae53c6e3c2c78109b5bf2435
hollowly <2499538506@qq.com>
2020-11-18 13:14:47 +0800

    enter

--- index/node.js/crud-起步/index.js
+++ index/node.js/crud-起步/index.js
@@ -3,15 +3,19 @@ const app = express();
 const router = require('./router')
 const bodyParser = require('body-parser')
 
-app.engine('html',require('express-art-template'))
-
+// 开放public文件夹
 app.use('/public/',express.static('./public/'))
 app.use('/node_modules/',express.static('../../../node_modules/'))
 
+
+// 配置template引擎模板
+app.engine('html',require('express-art-template'))
+
+// 配置bodyParser
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-// 把路由容器挂载到 app 服务中
 
+// 把路由容器挂载到 app 服务中
 app.use(router)
 
 app.listen(3000,() => {
