443b552660eca1a4cb8285339743b6d872c32005
hollowly <2499538506@qq.com>
2020-11-19 20:18:00 +0800

    enter

--- index/node.js/随便写的咯/router.js
+++ index/node.js/随便写的咯/router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req, res) => {
-	res.send('访问成功')
+	res.render('index.html')
 })
 
 router.get('/404', (req, res) => {
