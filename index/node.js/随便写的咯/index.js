a320b45e4d838f020ea14df38108243075e355b8
hollowly <2499538506@qq.com>
2020-11-19 20:48:54 +0800

    enter

--- index/node.js/随便写的咯/index.js
+++ index/node.js/随便写的咯/index.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const app = express()
+const router = require('./router')
 app.engine('html',require('express-art-template'))
 
-app.use('/public/',express.static('./public/'))
-
-app.get('/',(req, res) => {
-	res.render('index.html')
-})
-app.get('/post',(req, res) => {
-	res.render('post.html')
-})
-app.get('/404',(req, res) => {
-	res.render('404.html')
-})
+app.use(router)
 
 app.listen(3000,() => {
 	console.log('server is running http:127.0.0.1:3000');
--- index/node.js/随便写的咯/router.js
+++ index/node.js/随便写的咯/router.js
@@ -1,3 +1,22 @@
 const express = require('express')
 
 const router = express.Router()
+
+router.use('/public/',express.static('./public/'))
+
+router.get('/',(req, res) => {
+	res.render('index.html')
+})
+
+router.get('/post',(req, res) => {
+	res.render('post.html')
+})
+
+router.get('/404',(req, res) => {
+	res.render('404.html')
+})
+
+
+
+
+module.exports = router
