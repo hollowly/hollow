56e237be82b03cbc4fab244b62640adad73294e0
hollowly <2499538506@qq.com>
2020-11-20 10:30:27 +0800

    enter

--- index/node.js/随便写的咯/index.js
+++ index/node.js/随便写的咯/index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const router = require('./router')
-app.engine('html',require('express-art-template'))
+const http = require('http')
+const fs = require('fs')
 
-app.use(router)
-
-app.listen(3000,() => {
-	console.log('server is running http:127.0.0.1:3000');
+http.createServer((req, res) => {
+	if(req.url === '/') {
+		fs.readFile('./views/index.html',(err, data) => {
+			if(err) {
+				res.setHeader('Content-Type','text/plain;charset=utf-8')
+				return res.end('读取失败')
+			}
+			res.end(data)
+		})
+	} else if(req.url === '/post') {
+		res.end('post page')
+	} else if(req.url === '/404') {
+		res.end('404 page')
+	}
+}).listen(3000,() => {
+	console.log('server is running');
 })
