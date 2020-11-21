a2c2a584e023f30218bc11f55f2b7bfb8947e596
hollowly <2499538506@qq.com>
2020-11-21 23:11:04 +0800

    enter

--- index/node.js/tabbar/src/App.vue
+++ index/node.js/tabbar/src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="app">
+		
+		<span>{{value}}</span> <br>
+		<input type="text" v-model="value">
 		<tabbar>
 		</tabbar>
 		<router-view />
-
-
-	<div>
-		<span>{{value}}</span> <br>
-		<input type="text" v-model="value">
-
-	</div>
-		
   </div>
 </template>
 
@@ -19,7 +14,7 @@ import tabbar from './components/tabbar/tabbar'
 export default {
 	data() {
 		return {
-			value:123
+			value:123,
 		}
 	},
 	components: {
