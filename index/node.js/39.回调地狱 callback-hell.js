ede241274f2a2a58958c3a3c2dc1d70b7aada5b2
hollowly <2499538506@qq.com>
2020-11-22 16:29:47 +0800

    enter

--- index/node.js/39.回调地狱 callback-hell.js
+++ index/node.js/39.回调地狱 callback-hell.js
@@ -1,3 +1,44 @@
 var fs = require('fs')
 
-fs.readFile('./')
+// 这样的代码是无法保证执行顺序的
+// fs.readFile('./callback-hell/a.txt','utf8',(err, data) => {
+// 	if(err) {
+// 		throw err
+// 	}
+// 	console.log(data);
+// })
+// fs.readFile('./callback-hell/b.txt','utf8',(err, data) => {
+// 	if(err) {
+// 		throw err
+// 	}
+// 	console.log(data);
+// })
+// fs.readFile('./callback-hell/c.txt','utf8',(err, data) => {
+// 	if(err) {
+// 		throw err
+// 	}
+// 	console.log(data);
+// })
+
+
+
+// 所以我们需要嵌套来控制顺序
+// 如果有100个文件，那么久形成了回调地狱
+fs.readFile('./callback-hell/a.txt','utf8',(err, data) => {
+	if(err) {
+		throw err
+	}
+	console.log(data);
+	fs.readFile('./callback-hell/b.txt','utf8',(err, data) => {
+		if(err) {
+			throw err
+		}
+		console.log(data);
+		fs.readFile('./callback-hell/c.txt','utf8',(err, data) => {
+			if(err) {
+				throw err
+			}
+			console.log(data);
+		})
+	})
+})
