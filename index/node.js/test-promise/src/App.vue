947f2e482ef419327bbe9f5cee523bc85701934a
hollowly <2499538506@qq.com>
2020-11-23 12:59:00 +0800

    enter

---
+++ index/node.js/test-promise/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+		
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+
+<script>
+axios({
+	url:'http://123.207.32.32:8000/home/multidata',
+	method:'get'
+}).then(res => {
+	console.log(res);
+})
+</script>
+
+
+<style>
+
+</style>
