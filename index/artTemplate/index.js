b2d8033ccf5b29d84c0cef3fa81b648bd0f817d9
hollowly <2499538506@qq.com>
2020-11-23 21:45:41 +0800

    enter

---
+++ index/artTemplate/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+
+
+app.use('/public/', express.static(path.join(__dirname,'./public/')))
+app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
+
+app.engine('html',require('express-art-template'))
+
+app.set('views',path.join(__dirname,'./views'))
+
+
+app.get('/',(req, res) => {
+	res.render('index.html')
+})
+
+
+app.listen(3000,() => {
+	console.log('server is running','http://127.0.0.1:3000');
+})
