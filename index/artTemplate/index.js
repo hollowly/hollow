e892f184cb5c7d6cca577a4e07ad7c425cfc9ff8
hollowly <2499538506@qq.com>
2020-11-23 22:16:13 +0800

    enter

--- index/artTemplate/index.js
+++ index/artTemplate/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-
 const app = express()
-
 
 app.use('/public/', express.static(path.join(__dirname,'./public/')))
 app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
