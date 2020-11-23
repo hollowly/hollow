2f76a1363b1c6c6fd65fd94d14cf03aa958d9cc2
hollowly <2499538506@qq.com>
2020-11-23 14:14:43 +0800

    enter

---
+++ index/Vue/000.vue+axios+promise的应用.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+		<!-- <button @click='getData'>获取数据</button> -->
+		<table>
+			<tr v-for="(item, index) in bannerList" :key='index'>
+				<td>{{index + 1}}{{item.sort}}.</td>
+				<td><a :href="item.link">{{item.title}}</a></td>
+				<td><img :src="item.image" width="100"></td>
+			</tr>
+			<tr v-for="item in recommendList">
+				<td>{{item.sort}}.</td>
+				<td><a :href="item.link">{{item.title}}</a></td>
+				<td><img :src="item.image" width="100"></td>
+			</tr>
+		</table>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+		return {
+			bannerList:[],
+			recommendList:[]
+		}
+	},
+	created() {
+		Promise.all([
+			new Promise((resolve, reject) => {
+				axios({
+					url:'http://123.207.32.32:8000/home/multidata',
+					method:'get',
+				}).then(data => {
+					this.bannerList = data.data.data.banner.list
+				})
+			}),
+			new Promise((resolve, reject) => {
+				axios({
+					url:'http://123.207.32.32:8000/home/multidata',
+					method:'get',
+				}).then(data => {
+					this.recommendList = data.data.data.recommend.list
+				})
+			})
+		])
+	},
+}
+
+</script>
+
+
+<style>
+
+</style>
