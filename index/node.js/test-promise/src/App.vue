9d995a13b8b639d19699816c66b23655cf404573
hollowly <2499538506@qq.com>
2020-11-23 13:23:19 +0800

    enter

--- index/node.js/test-promise/src/App.vue
+++ index/node.js/test-promise/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-		
+			<ul>
+				<li v-for='(item, index) in list' :key='index'>{{item}}</li>
+			</ul>
   </div>
 </template>
 <script>
 import axios from 'axios'
 
 export default {
-  name: 'App'
+  data() {
+		return {
+			list:['111','222']
+		}
+	},
+	computed: {
+		
+	},
 }
 
 axios({
 	url:'http://123.207.32.32:8000/home/multidata',
-	method:'get'
+	method:'get',
 }).then(res => {
-	console.log(res);
+	this.list = res.data.data.banner.list
+	console.log(this.list);
+}).catch(err => {
+	console.log(err);
 })
 
 </script>
