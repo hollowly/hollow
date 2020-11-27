e252308c3e9c4a3b13a77c655e3b1c9763db11a9
hollowly <2499538506@qq.com>
2020-11-27 21:28:52 +0800

    enter

--- index/node.js/999.express NodeJS.js
+++ index/node.js/999.express NodeJS.js
@@ -1,13 +1,20 @@
 const express = require('express')
-
 const app = express()
 
+// 必须配置这个才能使用 render 来进行渲染 html
+// render 默认访问 views 中的文件
 app.engine('html',require('express-art-template'))
+
+// 设置 render 默认访问的文件夹
+app.set('views', __dirname + '/views');
 
 app.get('/',(req, res) => {
 	res.render('index.html')
 })
 
+app.get('/post',(req, res) => {
+	res.render('post.html')
+})
 
 app.listen(3000,() => {
 	console.log('server is running');
--- index/node.js/随便写的咯/index.js
+++ index/node.js/随便写的咯/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const router = require('./router')
 
+// 必须配置这个才能使用 render 来进行渲染 html
 app.engine('html',require('express-art-template'))
-
 app.use('/public/',express.static('./public/'))
 
 app.use(router)
