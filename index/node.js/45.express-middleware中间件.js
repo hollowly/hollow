769c3492f2813c85d497334853ff2d1a53de3fb9
hollowly <2499538506@qq.com>
2020-11-28 20:51:08 +0800

    enter

--- index/node.js/45.express-middleware中间件.js
+++ index/node.js/45.express-middleware中间件.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 // 中间件：处理请求的，本质就是一个函数
 // 在 express 中，对中间件有几种分类
 
@@ -11,62 +12,87 @@ const app = express()
 	// next	 下一个中间件：是一个方法，用来调用下一个中间件的
 
 	// 全局中间件
-app.use((req, res, next) => {
-	console.log('1')
-	next()
-})
-app.use((req, res, next) => {
-	console.log('2')
-	next()
-})
-app.use((req, res, next) => {
-	console.log('3')
-	next()
-})
+// app.use((req, res, next) => {
+// 	console.log('1')
+// 	next()
+// })
+// app.use((req, res, next) => {
+// 	console.log('2')
+// 	next()
+// })
+// app.use((req, res, next) => {
+// 	console.log('3')
+// 	next()
+// })
 
-// 以 /xxx 开头
-app.use('/a',(req, res, next) => {
-	console.log('a')
-	next()
-})
+// // 以 /xxx 开头
+// app.use('/a',(req, res, next) => {
+// 	console.log('a')
+// 	next()
+// })
 
-app.use('/b',(req, res, next) => {
-	console.log('b')
-	next()
-})
+// app.use('/b',(req, res, next) => {
+// 	console.log('b')
+// 	next()
+// })
 
-// 除了以上中间件之外，还有一种最常用的，严格匹配请求方法和请求路径的中间件
-// app.get
-// app.post
+// // 除了以上中间件之外，还有一种最常用的，严格匹配请求方法和请求路径的中间件
+// // app.get
+// // app.post
 
-app.use((req, res, next) => {
-	console.log('1')
-	next()
-})
+// app.use((req, res, next) => {
+// 	console.log('1')
+// 	next()
+// })
 
+// app.get('/',(req, res, next) => {
+// 	console.log('/')
+// })
+
+// app.get('/a',(req, res, next) => {
+// 	console.log('/a')
+// 	next()
+// })
+
+// app.use((req, res, next) => {
+// 	console.log('/2')
+// 	next()
+// })
+
+
+// // 相同的也会继续执行，不会被覆盖
+// app.get('/abc',(req, res, next) => {
+// 	console.log('abc')
+// 	next()
+// })
+// app.get('/abc',(req, res, next) => {
+// 	console.log('abc')
+// 	next()
+// })
+
+
+// 配置了 next(err) ，一旦服务器错误(500)就会跳转到
+// 全局错误处理中间件(带有4个参数的)
 app.get('/',(req, res, next) => {
-	console.log('/')
-})
-
-app.get('/a',(req, res, next) => {
-	console.log('/a')
-	next()
-})
-
-app.use((req, res, next) => {
-	console.log('/2')
-	next()
+	fs.readFile('./fdgdg/gsgrdgh0',(err, data) => {
+		if(err) {
+			res.status(500).send('server is error.')
+			next(err)
+		}
+	})
 })
 
 
-// 相同的也会继续执行，不会被覆盖
-app.get('/abc',(req, res, next) => {
-	console.log('abc')
-	next()
-})
-app.get('/abc',(req, res, next) => {
-	console.log('abc')
-	next()
+// 配置一个处理 404 的中间件
+// app.use((req, res, next) => {
+// 	res.render('404.html')
+// })
+
+// 配置一个全局错误处理中间件
+app.use((err, req, res, next) => {
+	if(err) {
+		console.log('app error handler');
+	}
 })
 
 app.listen(3000,() => {
