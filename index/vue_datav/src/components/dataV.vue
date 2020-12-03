5b68f800e2b60f8b7522ab804432f2f71a0da862
hollowly <2499538506@qq.com>
2020-12-03 21:02:44 +0800

    enter

--- index/vue_datav/src/components/dataV.vue
+++ index/vue_datav/src/components/dataV.vue
@@ -4,19 +4,19 @@
 		<dv-border-box-11 class="box">
 			<!-- <dv-full-screen-container> -->
 				<dv-loading>Loading...</dv-loading>
-				<dv-decoration-5 style="width:300px;height:40px;" />
-				<dv-decoration-1 style="width:200px;height:50px;" />
-				<dv-decoration-5 style="width:300px;height:40px;" />
-				<dv-decoration-1 style="width:200px;height:50px;" />
-				<dv-decoration-5 style="width:300px;height:40px;" />
-				<dv-decoration-1 style="width:200px;height:50px;" />
+				<dv-decoration-5 />
+				<dv-decoration-1 />
+				<dv-decoration-5 />
+				<dv-decoration-1 />
+				<dv-decoration-5 />
+				<dv-decoration-1 />
 
 				<dv-decoration-9 style="width:150px;height:150px;">66%</dv-decoration-9>
-				<dv-decoration-11 style="width:200px;height:60px;">dv-decoration-11</dv-decoration-11>
+				<dv-decoration-11>dv-decoration-11</dv-decoration-11>
 				<dv-decoration-12 style="width:150px;height:150px;"></dv-decoration-12>
 				<!-- 桃花运 -->
-				<dv-charts :option="option" style="width:300px;height:300px;" />
-				<dv-active-ring-chart :config="configBar" style="width:300px;height:300px" />
+				<dv-charts :option="option" class="charts" />
+				<dv-active-ring-chart :config="configBar" class="configBar"/>
 				<dv-capsule-chart :config="configBar" style="width:300px;height:200px" class="row-data-bar"/>
 				<!-- 水位图 -->
 				<dv-water-level-pond :config="configWater" style="width:150px;height:200px" />
@@ -27,6 +27,10 @@
 				<!-- 锥形图 -->
 				<dv-conical-column-chart :config="configCone" style="width:400px;height:200px;margin-top:-230px;"/>
 			<!-- </dv-full-screen-container> -->
+			<!-- 数字翻牌器 -->
+			<!-- <dv-digital-flop :config="config1" style="width:150px;height:100px;" /> -->
+			<!-- 轮播表 -->
+			<dv-scroll-board :config="configTable" class="configTable"/>
 		</dv-border-box-11>
 	</div>
 </template>
@@ -95,15 +99,17 @@ export default {
 			},
 			
 			configWater: {
-				data: [66],
+				data: [65, 45],
 				shape: 'roundRect',
 			},
+
 			configProgress: {
 				value: 66,
 				borderWidth: 5,
 				borderRadius: 10,
 				borderGap: 5
 			},
+
 			configCone:{
 				data: [
 				{
@@ -145,13 +151,30 @@ export default {
 				'http://datav.jiaminghi.com/img/conicalColumnChart/7st.png',
 			],
 			showValue: true
+			},
+
+			configTable: {
+				data: [
+					['行1列1', '行1列2', '行1列3'],
+					['行2列1', '行2列2', '行2列3'],
+					['行3列1', '行3列2', '行3列3'],
+					['行4列1', '行4列2', '行4列3'],
+					['行5列1', '行5列2', '行5列3'],
+					['行6列1', '行6列2', '行6列3'],
+					['行7列1', '行7列2', '行7列3'],
+					['行8列1', '行8列2', '行8列3'],
+					['行9列1', '行9列2', '行9列3'],
+					['行10列1', '行10列2', '行10列3']
+  			]
 			}
 		}
 	}
+
 }
 </script>
 
-<style>
+
+<style scoped>
 	.barbox {
 		margin: 0 auto;
 		margin-left:10px;
@@ -160,20 +183,42 @@ export default {
 	.box {
 		width: 100%;
 	}
-	div.dv-decoration-1,div.dv-decoration-5 {
+
+	div.dv-decoration-1 {
 		display: inline-block;
+	}
+	div.dv-decoration-1 {
+		width:200px;
+		height:50px;
+		display: inline-block;
+	}
+	div.dv-decoration-5 {
+		width:300px;
+		height:40px;
+		display: inline-block;
+	}
+	div.dv-decoration-11 {
+		width:200px;
+		height:60px;
+		transform: translateY(20px);
 	}
 	div{
 		/* display: block; */
 		float: left !important;
 	}
 
-
-	div.dv-charts-container {
-		display: inline;
+	/* 图标 */
+	.charts,.configBar {
+		width:300px;
+		height:300px;
 	}
 
 	.row-data-bar {
 		margin-top: 45px;
+	}
+	.configTable {
+		width:500px;
+		height:220px;
+		transform: translate(480px,-160px);
 	}
 </style>
