d93bb0b34c2d8db76752f117d4c48b394a46f98b
hollowly <2499538506@qq.com>
2020-12-04 11:29:57 +0800

    enter

--- index/vue_datav/src/router/index.js
+++ index/vue_datav/src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
+const flyline = () =>import('../components/flyline')
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
 	},
+	{
+		path:'/flyline',
+		components:flyline,
+	}
 ]
 
 
