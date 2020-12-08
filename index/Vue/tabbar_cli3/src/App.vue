579121db2448f98ee8515ee429a80a104eb31246
hollowly <2499538506@qq.com>
2020-12-08 15:12:26 +0800

    enter

--- index/Vue/tabbar_cli3/src/App.vue
+++ index/Vue/tabbar_cli3/src/App.vue
@@ -1,77 +1,19 @@
-o<template>
+<template>
   <div id="app">
-		<div id='tabbar'>
-			<router-link to='/categories'>
-				<div class="tabbar-item active">
-					<img src="./assets/img/tabbar/categories.png">
-					<p>首页</p>
-				</div>
-			</router-link>
-			
-			<router-link to='/shopcart'>
-			<div class="tabbar-item">
-				<img src="./assets/img/tabbar/shopcart.png">
-				<p>购物车</p>
-			</div>
-			</router-link>
-
-			<router-link to='/home'>
-			<div class="tabbar-item">
-				<img src="./assets/img/tabbar/home.png">
-				<p>关于</p>
-			</div>
-			</router-link>
-
-			<router-link to='/profile'>
-			<div class="tabbar-item">
-				<img src="./assets/img/tabbar/profile.png">
-				<p>我的</p>
-			</div>
-			</router-link>
-
-		</div>
-
+		<tabbar />
 		<router-view />
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
+import tabbar from './components/tabbar'
 
 export default {
-	// mounted：渲染后
-	mounted() {
-		$(function() {
-			$('.tabbar-item').click(function() {
-				$('.tabbar-item').removeClass('active');
-				$(this).addClass('active')
-			})
-		})
-	},
-
+	components: {
+		tabbar
+	}
 }
 </script>
-<style scoped>
+<style>
 	@import url("./assets/css/base.css");
-	#tabbar {
-		position: fixed;
-		display:flex;
-		right: 0;
-		left: 0;
-		bottom: 0;
-		height: 49px;	
-		background: #eee;
-	}
-	a {
-		flex: 1;
-		text-align: center;
-	}
-	.tabbar-item > img {
-		width: 25px;
-		height: 25px;
-		border-radius: 25px;
-	}
-	.active {
-		color: red;
-	}
 </style>
---
+++ index/Vue/tabbar_cli3/src/components/tabbar.vue
@@ -0,0 +1,88 @@
+<!--  -->
+<template>
+	<div id='tabbar'>
+		<router-link to='/categories'>
+			<div class="tabbar-item active">
+				<img src="../assets/img/tabbar/categories.png">
+				<img src="../assets/img/tabbar/categories_active.png" class="hidden">
+				<p>首页</p>
+			</div>
+		</router-link>
+		
+		<router-link to='/shopcart'>
+		<div class="tabbar-item">
+			<img src="../assets/img/tabbar/shopcart.png">
+			<img src="../assets/img/tabbar/shopcart_active.png" class="hidden">
+			<p>购物车</p>
+		</div>
+		</router-link>
+
+		<router-link to='/home'>
+		<div class="tabbar-item">
+			<img src="../assets/img/tabbar/home.png">
+			<img src="../assets/img/tabbar/home_active.png" class="hidden">
+			<p>关于</p>
+		</div>
+		</router-link>
+
+		<router-link to='/profile'>
+		<div class="tabbar-item">
+			<img src="../assets/img/tabbar/profile.png">
+			<img src="../assets/img/tabbar/profile_active.png" class="hidden">
+			<p>我的</p>
+		</div>
+		</router-link>
+	</div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+	data () {
+		return {
+
+		}
+	},
+	// mounted：渲染后
+	mounted() {
+		$(function() {
+			$('.tabbar-item').click(function() {
+				$('.tabbar-item').removeClass('active');
+				$(this).addClass('active')
+
+				$(this).children('img').eq(0).hide()
+				$(this).children('img').eq(1).toggleClass()
+			})
+		})
+	},
+
+}
+
+</script>
+
+<style scoped>
+	#tabbar {
+		position: fixed;
+		display:flex;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		height: 49px;	
+		background: #eee;
+	}
+	a {
+		flex: 1;
+		text-align: center;
+	}
+	.tabbar-item > img {
+		width: 25px;
+		height: 25px;
+		border-radius: 25px;
+	}
+	.active {
+		color: red;
+	}
+	.hidden {
+		display: none;
+	}
+</style>
