e8f608733e3e9a3a25274fa0cea75e7bece2780f
hollowly <2499538506@qq.com>
2020-12-08 12:54:47 +0800

    enter

--- index/Vue/tabbar_cli3/src/App.vue
+++ index/Vue/tabbar_cli3/src/App.vue
@@ -1,9 +1,43 @@
-<template>
+o<template>
   <div id="app">
-    <router-view/>
+		<div id='tabbar'>
+			<div class="tabbar-item">
+				首页
+				<img src="./assets/img/tabbar/categories.png">
+				</div>
+			<div class="tabbar-item">
+				购物车
+				<img src="./assets/img/tabbar/shopcart.png">
+				</div>
+			<div class="tabbar-item">
+				关于
+				<img src="./assets/img/tabbar/home.png">
+				</div>
+			<div class="tabbar-item">
+				我的
+				<img src="./assets/img/tabbar/profile.png">
+				</div>
+		</div>
   </div>
 </template>
 
-<style scoped>
+<style>
+	@import url("./assets/css/base.css");
 
+	#tabbar {
+		position: fixed;
+		display:flex;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		height: 49px;	
+		background: #eee;
+	}
+	.tabbar-item {
+		flex: 1;
+		text-align: center;
+	}
+	.tabbar-item > img {
+		
+	}
 </style>
