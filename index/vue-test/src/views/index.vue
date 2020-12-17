05a301454a2e2d4cdf189be759290c212efcfcc5
hollowly <2499538506@qq.com>
2020-12-17 22:13:34 +0800

    enter

--- index/vue-test/src/views/index.vue
+++ index/vue-test/src/views/index.vue
@@ -1,10 +1,22 @@
 <!--  -->
 <template>
 	<div>
-		<router-link to='/home'>主页</router-link>
-		<router-link to='/about'>关于</router-link>
-		<router-link to='/profile'>我的</router-link>
+		<div id='sidebar'>
 
+
+			<div class="sidebar-header">
+				<a href="/" style="float:left"><img src="@/assets/img/logo.svg" width="40"></a>
+				<div class="title">
+					<router-link to='/home'>主页</router-link>
+					<router-link to='/about'>关于</router-link>
+					<router-link to='/profile'>我的</router-link>
+				</div>
+				<a href="" class='sidebar-more'>☰</a>
+			</div>
+
+
+
+		</div>
 		<router-view />
 	</div>
 </template>
@@ -22,5 +34,26 @@ export default {
 </script>
 
 <style scoped>
+	a {
+		display: inline-block;
+	}
+	#sidebar {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		border: 1px solid red;
+		font-weight: 700
+	}
+	.title {
+		float: left;
+	}
+	.title > a {
+		margin: 10px 20px;
+		font-size: 16px
+	}
 
+	.sidebar-more {
+		margin: 2px 20px;
+		font-size: 25px
+	}
 </style>
--- index/vue-test/src/views/profile.vue
+++ index/vue-test/src/views/profile.vue
@@ -1,7 +1,18 @@
 <!--  -->
 <template>
 	<div>
-		profile
+		<div id='profile'>
+			<div>
+				<h2>菜单</h2>
+				<ul>
+					<li><a href="季度精选">季度精选</a></li>
+					<li><a href="饮料">饮料</a></li>
+					<li><a href="美食">美食</a></li>
+					<li><a href="咖啡产品">咖啡产品</a></li>
+					<li><a href="商品">商品</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -18,5 +29,12 @@ export default {
 </script>
 
 <style scoped>
-
+	#profile {
+		border:1px solid yellow;
+		position: absolute;
+		top: 40%;
+	}
+	#profile > div {
+		border:1px solid red;
+	}
 </style>
