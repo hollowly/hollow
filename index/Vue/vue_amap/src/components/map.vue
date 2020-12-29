075495dc148ff4ed315bb6cef3bc07ba8413be4d
hollowly <2499538506@qq.com>
2020-12-29 14:15:57 +0800

    enter

---
+++ index/Vue/vue_amap/src/components/map.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div style="width: 100vw;height: 100vh" id="container"></div>
+  </div>
+</template>
+<script>
+
+import AMap from "AMap";
+export default {
+  name: "Index",
+  data() {
+    return {};
+  },
+  mounted() {
+    new AMap.Map("container", {
+      resizeEnable: true,
+      zoom: 11
+    });
+  }
+};
+</script>
