1854d2c6fb735530844a920264ae5b567b259309
hollowly <2499538506@qq.com>
2021-01-05 14:52:07 +0800

    enter

--- index/Vue/vue_amap/src/App.vue
+++ index/Vue/vue_amap/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-
+		<cmap></cmap>
   </div>
 </template>
 
 <script>
+import cmap from '@/components/map'
+export default {
+	components: {
+		cmap,
+	}
+}
 </script>
 
 <style>
