61faac3a868c432c7ce6f59b56322ae56a22ce40
hollowly <2499538506@qq.com>
2021-01-06 18:39:16 +0800

    enter

--- index/Vue/vue_node_mysql/src/server/services.js
+++ index/Vue/vue_node_mysql/src/server/services.js
@@ -2,24 +2,36 @@ const db = require('./db.js')
 exports.start = (req,res)=>{
 }
 // 登录注册处理
-exports.login = (req,res)=>{
-    let username = req.body.username
-    let pwd = req.body.password
-    // 查询语句
-    let sql = 'select * from login where username = ?'
-    db.base(sql,username,(result)=>{
-        if(!result.length){
-            return res.json({ status: 1, msg: '账户不存在，请先注册' })
-        }else{
-            // [ RowDataPacket { password: '123', username: 'admin', id: 1 } ]
-            if(result[0].password==pwd){
-                return res.json({ status: 1, msg: '登录成功' })
-            }
-            return res.json({ status: 1, msg: '密码错误' })
-        }
-    })
+exports.login = (req,res) => {
+	let username = req.body.username
+	let pwd = req.body.password
+	// 查询语句
+	let sql = 'select * from login where username = ?'
+	db.base(sql,username,(result)=>{
+		if(!result.length){
+			return res.json({ status: 1, msg: '账户不存在，请先注册' })
+		}else{
+			// [ RowDataPacket { password: '123', username: 'admin', id: 1 } ]
+			if(result[0].password == pwd){
+				return res.json({ status: 1, msg: '登录成功' })
+			}
+			return res.json({ status: 1, msg: '密码错误' })
+		}
+	})
 }
-exports.register = (req,res)=>{
-    console.log(req.body)
-    res.send('测试')
+// 注册处理
+exports.register = (req,res) => {
+    let username = req.body.username
+		// 查询语句
+		let selectsql = 'select * from login where username = ?'
+		// let createsql = ''
+		db.base(selectsql,username,(result)=>{
+			if(result.length){
+				return res.json({ status: 1, msg: '账户已存在，请登录' })
+			}else {
+				// db.naseP
+				return res.json({ status: 1, msg:' 注册成功 ' })
+			}
+	})
+  // res.send('测试sss')
 }
--- index/Vue/vue_node_mysql/src/views/register.vue
+++ index/Vue/vue_node_mysql/src/views/register.vue
@@ -1,16 +1,39 @@
-<!--  -->
 <template>
-  <div>
-    <h2>注册</h2>
-  </div>
+  <form>
+    <label for="username">账户:</label>
+    <input type="text" v-model="username" id="username" />
+    <br />
+    <label for="pwd">密码:</label>
+    <input type="text" v-model="pwd" id="pwd" />
+    <br />
+    <button @click.prevent="register">注册</button>
+    <div>{{ msg }}</div>
+  </form>
 </template>
-
 <script>
 export default {
   data() {
-    return {};
+    return {
+      username: "",
+      pwd: "",
+      msg: "", //接收数据
+    };
+  },
+  methods: {
+    register() {
+      this.$axios
+        .post(this.HOST + "/api/register", {
+          username: this.username,
+          password: this.pwd,
+        })
+        .then((result) => {
+          console.log(result.data);
+          this.msg = result.data.msg;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
 </script>
-
-<style scoped></style>
