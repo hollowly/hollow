44ff556b657d2d4f8d69e2b979f4b88e0f6d9458
hollowly <2499538506@qq.com>
2021-01-06 19:10:08 +0800

    enter

--- index/Vue/vue_node_mysql/src/server/services.js
+++ index/Vue/vue_node_mysql/src/server/services.js
@@ -1,17 +1,17 @@
 const db = require('./db.js')
-exports.start = (req,res)=>{
+exports.start = (req,res) => {
 }
-// 登录注册处理
+// 登录处理
 exports.login = (req,res) => {
 	let username = req.body.username
 	let pwd = req.body.password
 	// 查询语句
 	let sql = 'select * from login where username = ?'
-	db.base(sql,username,(result)=>{
-		if(!result.length){
+	
+	db.base(sql,username,(result) => {
+		if(!result.length) {
 			return res.json({ status: 1, msg: '账户不存在，请先注册' })
 		}else{
-			// [ RowDataPacket { password: '123', username: 'admin', id: 1 } ]
 			if(result[0].password == pwd){
 				return res.json({ status: 1, msg: '登录成功' })
 			}
@@ -21,16 +21,20 @@ exports.login = (req,res) => {
 }
 // 注册处理
 exports.register = (req,res) => {
-    let username = req.body.username
+		let username = req.body.username
+		let pwd = req.body.password
 		// 查询语句
 		let selectsql = 'select * from login where username = ?'
-		// let createsql = ''
-		db.base(selectsql,username,(result)=>{
-			if(result.length){
-				return res.json({ status: 1, msg: '账户已存在，请登录' })
-			}else {
-				// db.naseP
-				return res.json({ status: 1, msg:' 注册成功 ' })
+		let insertsql = `insert into login(username,password)values('${username}','${pwd}')`
+
+		db.base(selectsql,username,(result) => {
+			if(result.length) {
+				return res.json({ status: 1, msg: '账户已存在，请返回登录' })
+			} else {
+				db.base(insertsql, (result) => {
+					console.log(result);
+					return res.json({ status: 1, msg:' 注册成功 ' })
+				})
 			}
 	})
   // res.send('测试sss')
