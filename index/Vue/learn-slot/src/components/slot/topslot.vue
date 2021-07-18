8f488810e47a0185b008f4c428402694aa7f2bdc
hollowly <2499538506@qq.com>
2021-07-18 18:59:28 +0800

    enter

---
+++ index/Vue/learn-slot/src/components/slot/topslot.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div>
+    <slot name="a">1111</slot>
+    <slot name="b">2222</slot>
+    <slot name="c">3333</slot>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs, onMounted } from "vue";
+export default {
+  setup(props) {
+    // 定义的参数或变量
+    const state = reactive({
+      mount: "onMounted",
+    });
+
+    // 方法写在下面 ↓=
+
+    // 要暴露的参数或方法 ↓
+    return { ...toRefs(state) };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ index/Vue/learn-slot/src/views/home.vue
@@ -0,0 +1,39 @@
+<!--  -->
+<template>
+  <div>
+    <topslot>
+      <template v-slot:a>
+        <div>
+          aaaaaaaaaaaaa
+        </div>
+      </template>
+      <template v-slot:b>
+        <span>bbbbbbbbbbbbbbbbbb</span>
+      </template>
+      <template v-slot:c>
+        <p>ccccccccccccccccccccc</p>
+      </template>
+    </topslot>
+  </div>
+</template>
+
+<script>
+import topslot from "@/components/slot/topslot";
+import { reactive, toRefs, onMounted } from "vue";
+export default {
+  setup(props) {
+    // 定义的参数或变量
+    const state = reactive({});
+
+    // 方法写在下面 ↓
+
+    // 要暴露的参数或方法 ↓
+    return { ...toRefs(state) };
+  },
+  components: {
+    topslot,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
